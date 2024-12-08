5095f462b21af938a27f018c4fc46ed358d3698b
brian kimutai <korosbrian574@gmail.com>
2024-12-08 09:57:27 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import find_packages,setup
 from typing import List
-HYPHEN_DOT_E='-e .'
-
-def get_requirements(file_path:str)->list[str]:
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
     with open(file_path)as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=[req.replace('\n',"")for req in requirements]
         
-        if HYPHEN_DOT_E in requirements:
-            requirements.remove(HYPHEN_DOT_E)
-            
-    return requirements    
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
-HYPHEN_DOT_E='-e .'
+    return requirements  
+        
+        
+
 setup(
-    name='Stroke-Disease-Prediction',
-    version='0.0.0.0'
-    author='BrianSiele',
+    name='Stroke Disease Prediction',
+    version='0.0.0.0',
+    author='BrainSiele',
     author_email='korosbrian574@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
+    
     
 )
